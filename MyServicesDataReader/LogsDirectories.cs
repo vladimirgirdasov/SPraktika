5814a1a390aae5753b7be6bfb65a47bc6bf8ad2c
vladimirgirdasov <anahim46510@gmail.com>
2016-07-25 16:13:48 +0400

    MyServiceDataReader. +(Config Safe Reader Writer)

---
+++ MyServicesDataReader/LogsDirectories.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyServicesDataReader
+{
+    public static class LogsDirectories
+    {
+        public static string DirectoryCurrency = @"";
+        public static string DirectoryWeather = @"";
+
+        private static string ConfigName = @"AppConfig.conf";
+
+        public static void Read_and_Check_Config()
+        {
+            if (File.Exists(ConfigName))
+            {
+                try
+                {
+                    var data = File.ReadAllLines(ConfigName);
+                    DirectoryCurrency = data[0];
+                    DirectoryWeather = data[1];
+
+                    if (!IsCurrencyDirectory(DirectoryCurrency))
+                        throw new Exception("В вашей последней директории валют не найдено логов. Директория сброшена");
+
+                    if (!IsWeatherDirectory(DirectoryWeather))
+                        throw new Exception("В вашей последней директории погоды не найдено логов. Директория сброшена");
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, "Ошибка чтения конфига директорий");
+                    SetDefaultDirectories();
+                }
+            }
+        }
+
+        public static void Set_and_Write_Config(string dirCurr, string dirWeather)
+        {
+            if (dirCurr != null)
+                DirectoryCurrency = dirCurr;
+            if (dirWeather != null)
+                DirectoryWeather = dirWeather;
+            try
+            {
+                if (File.Exists(ConfigName))
+                    File.Delete(ConfigName);
+
+                string[] data = { DirectoryCurrency, DirectoryWeather };
+                File.WriteAllLines(ConfigName, data);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Ошибка записи конфига директорий");
+                SetDefaultDirectories();
+            }
+        }
+
+        public static void SetDefaultDirectories()
+        {
+            DirectoryCurrency = @"";
+            DirectoryWeather = @"";
+        }
+
+        public static bool IsCurrencyDirectory(string dirCurr)
+        {
+            string[] all_logs = Directory.GetFiles(dirCurr, "CurrenciesLog__??.??.????.xml", SearchOption.TopDirectoryOnly);
+            if (all_logs.Count() > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool IsWeatherDirectory(string dirWeather)
+        {
+            string[] all_logs = Directory.GetFiles(dirWeather, "WeatherLog__??.??.????.xml", SearchOption.TopDirectoryOnly);
+            if (all_logs.Count() > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ MyServicesDataReader/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyServicesDataReader
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LogsDirectories.Read_and_Check_Config();
+            tbFolderCurrencyLogs.Text = LogsDirectories.DirectoryCurrency;
+            tbFolderWeatherLogs.Text = LogsDirectories.DirectoryWeather;
+        }
+
+        private void bFolderCurrencyLogs_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            if (dialog.ShowDialog(this).GetValueOrDefault())
+            {
+                if (LogsDirectories.IsCurrencyDirectory(dialog.SelectedPath))
+                {
+                    tbFolderCurrencyLogs.Text = dialog.SelectedPath;
+                    LogsDirectories.Set_and_Write_Config(dialog.SelectedPath, null);
+                    MessageBox.Show("Логи курсов валют в директории найдены, Конфиг обновлен", "OK");
+                }
+                else
+                {
+                    MessageBox.Show("Тут файлы логов не найдены", "Ошибка определения директории");
+                }
+            }
+        }
+
+        private void bFolderWeatherLogs_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            if (dialog.ShowDialog(this).GetValueOrDefault())
+            {
+                if (LogsDirectories.IsWeatherDirectory(dialog.SelectedPath))
+                {
+                    tbFolderWeatherLogs.Text = dialog.SelectedPath;
+                    LogsDirectories.Set_and_Write_Config(null, dialog.SelectedPath);
+                    MessageBox.Show("Логи погоды в директории найдены, Конфиг обновлен", "OK");
+                }
+                else
+                {
+                    MessageBox.Show("Тут файлы логов не найдены", "Ошибка определения директории");
+                }
+            }
+        }
+    }
+}
